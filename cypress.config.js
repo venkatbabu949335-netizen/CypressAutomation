01f50eb9c07db84dd6bda214170dd23bed61b224
Swetha <love@Thank-You-2.local>
2026-05-04 08:12:28 +0530

    add

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,13 @@ module.exports = defineConfig({
 
   e2e: {
     supportFile: "cypress/support/e2e.js", 
+     reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true
+  },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
