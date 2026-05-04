3c751e54ccc6f840663d390d11693d0b4396066a
Swetha <love@Thank-You-2.local>
2026-05-04 07:55:51 +0530

    Added

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    supportFile: "cypress/support/e2e.js", 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
