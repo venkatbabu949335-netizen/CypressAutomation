33f74a70453da78ba8bddc96d9657c46091fac6e
venkat.kakani <venkatbabu949335@gmail.com>
2026-05-05 15:53:25 +0530

    che

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,30 @@
+describe("Login Tests (CSV Driven)", () => {
+    let data = [];
+
+  before(() => {
+    cy.loadCsv("login.csv").then((csvData) => {
+      data = csvData;
+    });
+  });
+
+  it("Run login tests from CSV", () => {
+
+   
+
+    data.forEach((row) => {
+
+      cy.log(row.testDescription)
+
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+      cy.get('[placeholder="Username"]').type(row.username)
+      cy.get('[placeholder="Password"]').type(row.password)
+      cy.get('.oxd-button').click();
+        cy.contains('Dashboard').should('be.visible')
+     cy.reload();
+
+    })
+
+  })
+
+})
